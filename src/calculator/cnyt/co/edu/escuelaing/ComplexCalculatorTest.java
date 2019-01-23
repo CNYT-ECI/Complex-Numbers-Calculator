7738780e472418182b876b882b1ee8028ebc0fcd
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2019-01-23 10:55:19 -0500

    Basic operations

---
+++ src/calculator/cnyt/co/edu/escuelaing/ComplexCalculatorTest.java
@@ -0,0 +1,22 @@
+package calculator.cnyt.co.edu.escuelaing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplexCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void substract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void division() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void product() {
+    }
+}
